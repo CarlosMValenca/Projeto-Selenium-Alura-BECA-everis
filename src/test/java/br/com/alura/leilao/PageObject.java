99d39c18811096774896989251d62fd7ed30e492
Carlos M Valenca <cvalenca@everis.com>
2021-10-11 14:10:03 -0300

    Refatorando

---
+++ src/test/java/br/com/alura/leilao/PageObject.java
@@ -0,0 +1,26 @@
+package br.com.alura.leilao;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageObject {
+
+	protected WebDriver browser;
+
+	public PageObject(WebDriver browser) {
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/cvalenca/OneDrive - everis/Documentos/Treinamento_Alura/Selenium/selenium/drivers/chromedriver.exe");
+
+		if (browser == null) {
+			this.browser = new ChromeDriver();
+		} else {
+			this.browser = browser;
+		}
+	}
+	
+	public void fechar() {
+		this.browser.quit();
+		
+	}
+
+}
